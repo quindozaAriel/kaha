968bfc7e1b90bf428b758a1573e0ee91ebfff2c5
Ariel G. Quindoza Jr <quindozajrariel@gmail.com>
2020-09-23 11:09:49 +0800

    Add: Integrate Vue.js

---
+++ resources/js/components/Login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col align-self-center">
+          asdsds
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
