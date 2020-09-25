8d17d542147cd8ea8a352767f3ca67cdf3cdda8d
Ariel G. Quindoza Jr <quindozajrariel@gmail.com>
2020-09-25 16:25:56 +0800

    add: add login form

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -2,11 +2,33 @@
   <div class="div-wrapper">
     <div class="login-box">
       <label class="system-title">Kaha</label>
-      <button class="menu-btn" v-on:click="isHidden = false">GET STARTED</button>
+      <button class="menu-btn" v-on:click="isHidden = false">
+        GET STARTED
+      </button>
       <button class="menu-btn">DOCUMENTATION</button>
     </div>
     <div class="login-bg" v-if="!isHidden">
-      
+      <div class="navbr"></div>
+      <div class="login-div">
+        <div class="login-logo">
+          <label class="flip">K</label>
+          <label>K</label>
+        </div>
+        <input
+          type="text"
+          id="username"
+          class="login-input"
+          autocomplete="off"
+        />
+        <label for="username">USERNAME</label>
+
+        <div class="separator"></div>
+
+        <input type="password" id="password" class="login-input" />
+        <label for="password">PASSWORD</label>
+
+        <button class="btn-login" type="button">LOGIN</button>
+      </div>
     </div>
   </div>
 </template>
@@ -14,13 +36,11 @@
 <script>
 export default {
   data: () => {
-    return{
-      isHidden:true
-    }
+    return {
+      isHidden: false,
+    };
   },
-  method:() => {
-
-  }
+  method: () => {},
 };
 </script>
 
@@ -31,6 +51,7 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  font-family: Minimalistic;
 }
 
 .login-box {
@@ -44,7 +65,6 @@ export default {
   align-items: center;
   display: flex;
   flex-direction: column;
-  font-family: Minimalistic;
 
   .system-title {
     font-size: 100px;
@@ -57,6 +77,8 @@ export default {
     background: transparent;
     color: white;
     letter-spacing: 5px;
+    transition: 0.5s;
+    outline: 0;
   }
 
   .menu-btn:nth-child(2) {
@@ -66,7 +88,6 @@ export default {
     padding-right: 25px;
     padding-top: 14px;
     padding-bottom: 14px;
-    outline: none;
     box-shadow: 3px 3px 6px #5a4c37;
     font-weight: bold;
   }
@@ -75,19 +96,121 @@ export default {
     margin-top: 10px;
     font-size: 17px;
   }
+
+  .menu-btn:nth-child(2):hover {
+    background: #f7b865;
+  }
+
+  .menu-btn:nth-child(3):hover {
+    background: rgb(143, 142, 142);
+    padding: 10px;
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+
+  .menu-btn:active {
+    font-size: 10px;
+  }
 }
 
+.separator {
+  width: 100%;
+  height: 20px;
+}
 .login-bg {
   position: fixed;
   height: 100vh;
   width: 100%;
   background: linear-gradient(140deg, #cca585, #693f23);
-  animation:expand 3s;
+  animation: expand 3s;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
 }
 
-@keyframes expand{
-  from{clip-path: circle(100px)}
-  to{clip-path: circle(2000px)}
+.login-div {
+  width: 400px;
+  height: auto;
+  padding: 50px;
+  text-align: center;
+  .login-input {
+    display: block;
+    width: 100%;
+    height: 50px;
+    text-align: center;
+    background: transparent;
+    color: white;
+    border: 0px;
+    border-bottom: 1px solid rgb(235, 224, 206);
+    outline: none;
+    font-size: 25px;
+    letter-spacing: 2px;
+  }
+  label {
+    display: block;
+    font-size: 20px;
+    color: white;
+    margin-top: 10px;
+    letter-spacing: 5px;
+  }
+}
+
+.btn-login {
+  width: 100%;
+  height: 50px;
+  margin-top: 30px;
+  border: 0;
+  font-size: 20px;
+  letter-spacing: 5px;
+  font-weight: bold;
+  background: #b1815f;
+
+  color: rgb(241, 219, 219);
+  border-radius: 10px;
+  outline: none !important;
+  border: none !important;
+}
+
+.btn-login:hover {
+  background: #805c42;
+}
+
+.btn-login:active {
+  outline: none;
+  border: none;
+  font-size: 18px;
+}
+
+.login-logo {
+  .flip {
+    -moz-transform: scale(-1, 1);
+    -webkit-transform: scale(-1, 1);
+    -o-transform: scale(-1, 1);
+    -ms-transform: scale(-1, 1);
+    transform: scale(-1, 1) !important;
+  }
+  label {
+    display: inline-block;
+    font-size: 100px;
+  }
+}
+
+.navbr {
+  width: 100%;
+  height: 60px;
+  border: 1px solid red;
+  position: absolute;
+  top: 0;
+}
+
+@keyframes expand {
+  from {
+    clip-path: circle(100px);
+  }
+  to {
+    clip-path: circle(2000px);
+  }
 }
 
 /* Extra small devices (phones, 600px and down) */
@@ -96,7 +219,6 @@ export default {
     width: 400px;
     height: 400px;
     font-size: 50px;
-
   }
 
   .login-box {
